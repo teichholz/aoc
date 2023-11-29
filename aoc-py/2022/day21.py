753b13ec8b9201ff5ab04e2199c4612076b91013
Tim Eichholz <t.eichholz@reply.de>
2023-11-29 09:17:47 +0100

    python day 21

--- aoc-py/2022/day21.py
+++ aoc-py/2022/day21.py
@@ -18,33 +18,44 @@ def p1(n: str = 'root'):
     else:
         n1, op, n2 = msd[n]
         return int(eval(f"({p1(n1)} {op} {p1(n2)})"))
-print(p1('root'))
+print(p1())
 
 # part 2
-n1, _, n2 = msd['root']
-print(n1, n2) # n1 = n2 is what we want to solve for
-print(p1(n1), p1(n2))
+# n1, _, n2 = msd['root']
+# print(n1, n2) # n1 = n2 is what we want to solve for
+# print(p1(n1), p1(n2))
 
-msd['humn'] = 9141913668089
+# msd['humn'] = 3509819803065
 
-# 9141913668089 is too high
-# solves the equality equation for humn
-def solveForHumn():
+# n1, _, n2 = msd['root']
+# print(n1, n2) # n1 = n2 is what we want to solve for
+# print(p1(n1), p1(n2))
+
+def solveforhumn():
+    """
+    Solves the equality equation for humn\n
+    How to handle non commutative operators like / and - ?
+    ---
+     10 / x = 5 should become x = 10 / 5
+    ---
+     5 - x = 10 should become x = 5 - 10
+    """
     n1, _, n2 = msd['root']
     right = p1(n2) 
     path = findPathTo('humn')[1:] # i know humn is in my left tree so I cheat
 
     n = n1
-    tr = None # node to take on the other side of the equation
     for goLeft in path:
         n1, op, n2 = msd[n]
         if (goLeft):
             n = n1
-            tr = n2
+            right = int(eval(f"(right {inverse(op)} {p1(n2)})"))
         else:
             n = n2
-            tr = n1
-        right = eval(f"(right {inverse(op)} {p1(tr)})")
+            # we need to be smart here and be aware of non commutative operators like - and /
+            right = int(eval(f"({p1(n1)} {op} right)")) 
+                
+        
     return right
 
 def inverse(op: str):
@@ -53,22 +64,10 @@ def inverse(op: str):
     elif (op == '*'): return '/'
     elif (op == '/'): return '*'
 
-
-# quick check if my path works
-def followPath(ps: list[bool], root: str = 'root'):
-    n = root
-    for goLeft in ps:
-        if (goLeft):
-            n1, _, _ = msd[n]
-            n = n1
-        else:
-            _, _, n2 = msd[n]
-            n = n2
-    return n
-
-# find the path to a specific node. A path is either left or right (1, 0 / true, false)
-# we do this bottom up
 def findPathTo(s: str, root: str = 'root'):
+    """
+    Find the path to a specific node. A path is either left or right (1, 0 / true, false)
+    """
     if (s == root): return []
     for k, v in msd.items():
         if (isinstance(v, int)):
@@ -80,18 +79,3 @@ def findPathTo(s: str, root: str = 'root'):
             return findPathTo(k) + [True]
         elif (n2 == s):
             return findPathTo(k) + [False]
-    
-# finds occurences of s in m in regard to msd
-# humn appears once in the left side of my tree
-def findOccurences(s: str, root: str = 'root'):
-    if (isinstance(msd[root], int)):
-        return 0
-
-    n1, _, n2 = msd[root]
-
-    if (n1 == s):
-        return 1 + findOccurences(s, n2)
-    elif (n2 == s):
-        return 1 + findOccurences(s, n1)
-    else:
-        return findOccurences(s, n1) + findOccurences(s, n2)
