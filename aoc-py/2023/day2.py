8ab9aaee3e48b36c1bcfc33fdb47aca7847813ee
Tim Eichholz <t.eichholz@reply.de>
2023-12-04 20:25:07 +0100

    2023 day 2

--- aoc-py/2023/day2.py
+++ aoc-py/2023/day2.py
@@ -1,39 +1,31 @@
 import helpers as h 
 from functools import reduce
-import operator
-import re
+from collections import Counter
+from operator import mul, or_
 
 input = h.readdaylines(2, 2023)
 
 def parse(line):
     p = line.split(': ')[1]
     ps = p.split('; ')
-    return [dict(map(game, p.split(', '))) for p in ps]
+    return [dict(map(color, p.split(', '))) for p in ps]
 
-
-# '4 red' -> (red, 4)
-def game(str):
+def color(str):
+    """
+    '4 red' -> ('red', 4)
+    """
     amount, color = str.split(' ')    
     return (color, int(amount))
 
-bag = { 'red': 12, 'green': 13, 'blue': 14 }
-def isValid(set):
-    return not any([amount > bag[color] for color, amount in set.items()])
-
-sum = 0
-sum2 = 0
+sum, sum2 = 0, 0
+bag = Counter({ 'red': 12, 'green': 13, 'blue': 14 })
 for i, line in enumerate(input):
     g = parse(line)
+    max = reduce(or_, [Counter(set) for set in g])
 
-    if (all([isValid(set) for set in g])):
+    if (bag >= max):
         sum += i + 1
     
-    minBag = { 'red': 0, 'green': 0, 'blue': 0 }
-    for set in g:
-        for color, amount in set.items():
-            minBag[color] = max(minBag[color], amount)
-    sum2 += reduce(operator.mul, minBag.values())
+    sum2 += reduce(mul, max.values())
 
-
-print(sum) 
-print(sum2) 
+print(f'part 1: {sum}\npart 2: {sum2}') 
