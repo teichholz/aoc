804876718464d4b2dea91d586f1e65a21cf905b6
Tim Eichholz <t.eichholz@reply.de>
2024-12-17 18:38:40 +0100

    fix day 17

--- aoc-py/2024/day17.py
+++ aoc-py/2024/day17.py
@@ -8,16 +8,15 @@ lines = readday(17, 2024, example=False).strip()
 class Machine:
     regs: dict[str, int]
     prog: list["OP"]
+
     out: list[int]
     instr: int
-
-    jumped: bool = False
+    jumped: bool
 
     def exe(self):
         self.out = []
         while self.instr < len(self.prog):
             self.prog[self.instr].exe(self)
-            print(self)
             if self.jumped:
                 self.jumped = False
             else:
@@ -47,7 +46,7 @@ class OP:
 
 class ADV(OP):
     def exe(self, mach: Machine):
-        num, denom = mach.regs["A"], 2 ** self.combo(regs)
+        num, denom = mach.regs["A"], 2 ** self.combo(mach.regs)
         mach.regs["A"] = num // denom
 
 class BXL(OP):
@@ -60,9 +59,7 @@ class BST(OP):
 
 class JNZ(OP):
     def exe(self, mach: Machine):
-        if mach.regs["A"] == 0:
-            return
-        else:
+        if mach.regs["A"] != 0:
             mach.jumped = True
             mach.instr = self.operand // 2
 
@@ -76,12 +73,12 @@ class OUT(OP):
 
 class BDV(OP):
     def exe(self, mach: Machine):
-        num, denom = mach.regs["A"], 2 ** self.combo(regs)
+        num, denom = mach.regs["A"], 2 ** self.combo(mach.regs)
         mach.regs["B"] = num // denom
 
 class CDV(OP):
     def exe(self, mach: Machine):
-        num, denom = mach.regs["A"], 2 ** self.combo(regs)
+        num, denom = mach.regs["A"], 2 ** self.combo(mach.regs)
         mach.regs["C"] = num // denom
 
 def parse():
@@ -91,17 +88,15 @@ def parse():
     return {"A": a, "B": b, "C": c}, prog
 
 
-regs, prog = parse()
-
 def part1():
+    regs, prog = parse()
     opcodes = prog[::2]
     operands = prog[::-2][::-1]
     instrs = [op(operand) for op, operand in zip(map(op, opcodes), operands)]
-    machine = Machine(dict(regs), instrs, [], 0)
-    print(machine)
-    # machine.exe()
+    machine = Machine(dict(regs), instrs, [], 0, False)
+    machine.exe()
 
-    # return ",".join(map(str, machine.out))
+    return ",".join(map(str, machine.out))
 
 def op(opcode: int):
     mapping = {0: ADV, 1: BXL, 2: BST, 3: JNZ, 4: BXC, 5: OUT, 6: BDV, 7: CDV}
