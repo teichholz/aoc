f092a08bbd7fe047b92cd22e3dc2f162967845bc
Tim Eichholz <t.eichholz@reply.de>
2024-12-21 19:09:41 +0100

    day21

--- aoc-py/2024/day21.py
+++ aoc-py/2024/day21.py
@@ -1,93 +1,35 @@
-from typing import Callable, List
 from functools import cache
 from helpers import readdaylines
 
 codes = readdaylines(21, 2024, example=False)
 
 def part1():
-    complexity = 0
-    for code in codes:
-        print(f"DEBUGPRINT[58]: day21.py:9: code={code}")
-        path_len = length(code, 3)
-        print(f"DEBUGPRINT[59]: day21.py:11: path_len={path_len}")
-        num = int(code[:-1])
-        print(f"DEBUGPRINT[60]: day21.py:13: num={num}")
-        complexity += path_len * num
+    return sum(int(code[:-1]) * length(code, 3) for code in codes)
 
-    return complexity
+def part2():
+    return sum(int(code[:-1]) * length(code, 26) for code in codes)
 
-@cache
-def length(code: str, depth: int) -> int:
-    if depth == 0:
-        return len(code)
 
-    moves = zip("A" + code, code)
-    possible_paths = [paths(*move) for move in moves]
-
-    # all possible combinations
-    codes = list(possible_paths[0])
-    for ps in possible_paths[1:]:
-        new_codes = []
-        for code in codes:
-            for p in ps:
-                new_codes.append(code + p)
-        codes = new_codes
-
-    return min(length(code, depth - 1) for code in codes)
-
+NUMPAD = {'7': 0, '8': 1, '9': 2, '4': 1j, '5': 1 + 1j, '6': 2 + 1j,
+          '1': 2j, '2': 1 + 2j, '3': 2 + 2j, ' ': 3j, '0': 1 + 3j, 'A': 2 + 3j}
+DIRPAD = {' ': 0, '^': 1, 'A': 2, '<': 1j, 'v': 1 + 1j, '>': 2 + 1j}
 
 @cache
-def paths(start_let: str, end_let: str) -> list[str]:
-    pad = NUMPAD if start_let in NUMPAD and end_let in NUMPAD else DIRPAD
-    start = pad[start_let]
-    end = pad[end_let]
-
-    illegal = 3 if pad == NUMPAD else 0
-
-    diff = end - start
-    drow, dcol = int(diff.real), int(diff.imag)
+def path(start, end):
+    pad = NUMPAD if (start in NUMPAD and end in NUMPAD) else DIRPAD
+    diff = pad[end] - pad[start]
+    dcol, drow = int(diff.real), int(diff.imag)
     ver = ("^" * -drow) + ("v" * drow)
     hor = ("<" * -dcol) + (">" * dcol)
 
-    pref_ver = illegal.real == start.real  # same row
+    ill = pad[" "] - pad[start]
+    prefer_yy_first = (dcol > 0 or ill == dcol) and ill != drow * 1j
+    return (ver + hor if prefer_yy_first else hor + ver) + "A"
 
-    if pref_ver:
-        return [ver + hor + "A"]
-    else:
-        return [hor + ver + "A", ver + hor + "A"]
+@cache
+def length(code, depth):
+    if depth == 0:
+        return len(code)
 
-
-def coords_num(let: str):
-    rows_num = ["789", "456", "123", "0A"]
-    cols_num = ["741", "8520", "963A"]
-    return coord(rows_num, cols_num)(let)
-
-def coords_dir(let: str):
-    rows_dir = ["^A", "<v>"]
-    cols_dir = ["<", "^v", "A>"]
-    return coord(rows_dir, cols_dir)(let)
-
-
-def coord(rows: List[str], cols: List[str]) -> Callable[[str], complex]:
-    def inner(let: str):
-        row, col = -1, -1
-        for i, str in enumerate(rows):
-            if let in str:
-                row = i
-
-        for i, str in enumerate(cols):
-            if let in str:
-                col = i
-
-        if row == -1 or col == - 1:
-            raise Exception(f"Unknown coordinate {let}")
-
-        return row + col * 1j
-    return inner
-
-
-NUMPAD = {let: coords_num(let) for let in "0123456789A"}
-DIRPAD = {let: coords_dir(let) for let in "<>^vA"}
-
-def part2():
-    pass
+    return sum(length(path(code[i - 1], c), depth - 1)
+               for i, c in enumerate(code))
