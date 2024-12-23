aead35d66d42bbf4b71a652e55d854f1e1c3542b
Tim Eichholz <t.eichholz@reply.de>
2024-12-23 15:39:44 +0100

    day 23

---
+++ aoc-py/2024/day23.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+from helpers import readdaylines, profiler
+from itertools import combinations
+import networkx as nx
+
+lines = readdaylines(23, 2024, example=False)
+graph = defaultdict(list)
+for edge in lines:
+    v1, v2 = edge.split("-")
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+def cliques(graph, size=3):
+    for nodes in combinations(graph.keys(), size):
+        if all(v2 in graph[v1] for i, v1 in enumerate(nodes) for v2 in nodes[i + 1:]):
+            yield nodes
+
+@profiler
+def part1():
+    sum = 0
+    for clique in cliques(graph, size=250):
+        if any(v.startswith("t") for v in clique):
+            sum += 1
+
+    return sum
+
+@profiler
+def part2():
+    G = nx.Graph()
+    for node, neighbors in graph.items():
+        for neighbor in neighbors:
+            G.add_edge(node, neighbor)
+    print(G)
+
+    cliques = nx.find_cliques(G)
+    mx = max(cliques, key=len)
+    mx.sort()
+    return ",".join(mx)
