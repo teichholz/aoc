402c4ba3847698f913c673d6964e93f6b59f1a20
Tim Eichholz <t.eichholz@reply.de>
2024-12-25 13:44:06 +0100

    day14 part2

--- aoc-py/2024/day14.py
+++ aoc-py/2024/day14.py
@@ -1,16 +1,21 @@
-from helpers import readdaylines
-from dataclasses import dataclass
-from collections import defaultdict
 import re
+from collections import defaultdict
+from dataclasses import dataclass
+
+from joblib import Parallel, delayed
+
+from helpers import readdaylines
 
 lines = readdaylines(14, 2024, example=False)
 # W, H = 11, 7  # exclusive size
 W, H = 101, 103  # exclusive size
 
+
 @dataclass
 class Robot:
     pos: tuple[int, int]
     vec: tuple[int, int]
+
 
 def parse():
     parsed = []
@@ -20,6 +25,7 @@ def parse():
 
     return parsed
 
+
 def part1():
     seconds = 100
     robots = parse()
@@ -28,7 +34,10 @@ def part1():
     for robot in robots:
         pos_x, pos_y = robot.pos
         vec_x, vec_y = robot.vec
-        moved[wrap_around(pos_x + (vec_x * seconds), W), wrap_around(pos_y + (vec_y * seconds), H)].append(robot)
+        moved[
+            wrap_around(pos_x + (vec_x * seconds), W),
+            wrap_around(pos_y + (vec_y * seconds), H),
+        ].append(robot)
 
     # lo inclusive, hi exclusize
     lo_x, hi_x = (0, W // 2), (W // 2 + 1, W)
@@ -45,27 +54,41 @@ def part1():
 
     return safe_factor
 
+
 def part2():
     """
     I won't do this. Maybe just check if no robots overlap.
     """
-    seconds = 100
     robots = parse()
 
-    for secs in range(seconds):
-        print(f"DEBUGPRINT[6]: day14.py:52: secs={secs}")
-        moved = defaultdict(list)
-        for robot in robots:
-            pos_x, pos_y = robot.pos
-            vec_x, vec_y = robot.vec
-            moved[wrap_around(pos_x + (vec_x * seconds), W), wrap_around(pos_y + (vec_y * seconds), H)].append(robot)
-        d(moved)
+    return min(
+        Parallel(n_jobs=-1, verbose=1)(
+            delayed(check)(seconds, robots) for seconds in range(1, 1_000_000)
+        )
+    )
+
+
+def check(seconds, robots):
+    moved = defaultdict(list)
+    for robot in robots:
+        pos_x, pos_y = robot.pos
+        vec_x, vec_y = robot.vec
+        moved[
+            wrap_around(pos_x + (vec_x * seconds), W),
+            wrap_around(pos_y + (vec_y * seconds), H),
+        ].append(robot)
+    if all(len(cell) <= 1 for cell in moved.values()):
+        return seconds
+
+    return 100_000_000
+
 
 def d(moved, end=""):
     for y in range(H):
         for x in range(W):
             print(f"{len(moved[(x, y)])}", end=end)
         print()
+
 
 def wrap_around(num, size):
     return num % size
